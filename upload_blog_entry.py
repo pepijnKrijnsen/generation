c78f0acb6dca8eb1311be27e5e3a95e818ae6b1a
Pepijn Krijnsen <pepijn@krijnsen.com>
2020-09-24 21:15:05 +0100

    Rough draft

--- upload_blog_entry.py
+++ upload_blog_entry.py
@@ -2,17 +2,47 @@
 # 
 # Generate a full HTML page from a template HTML plus a completed blog. It's
 # a script, so I can probably keep it procedural from start to finish.
-# Script takes the completed blog file as the only argument.
 #
 # STEPS:
-# 1. Check if the provided argument is a valid file
+# 1. Check that there's exactly 1 file in ~/projects/blog/completed/
 # 2. Read the entire file into memory
-# 3. Read the template.html file up to a placeholder point
-# 4. Move cursor position to that point
-# 5. Paste the contents of the blog at that location
+# 3. Read the template.html file into two variables: top and bottom
+# 4. Construct a new blog file from template_top + blog + template_bottom
 #
-import sys
+import os
 
 def main():
-    f = open("blog_date.html", "x")
+    content_dir = "/home/pepijn/projects/blog/completed/"
+    # check that there's exactly one file in content_dir
+    if len(os.listdir(content_dir)) != 1:
+        print("Please ensure " + content_dir + " contains exactly 1 file")
+        quit()
+    c = os.listdir(content_dir)
+    content_name = c[0]
+    content_file = content_dir + content_name
+    # read that file into a variable and close it
+    f = open(content_file)
+    blog = f.read()
+#    print(blog)
     f.close()
+    # read both halves of template.html into separate variables
+    template_file = "/home/pepijn/coding/projects/writesite/dev/lib/template.html"
+    f = open(template_file)
+    line = ""
+    template_top = ""
+    template_bottom = ""
+    while "START content" not in line:
+        line = f.readline()
+        template_top += line
+    template_top += "\n"
+    template_bottom = f.read()  # read from last cursor position to end
+    f.close()
+    # generate a complete blog page
+    new_blog = template_top + blog + template_bottom
+    # create the new blog file
+    blog_dir = "/home/pepijn/coding/projects/writesite/dev/blog/"
+    f = open(blog_dir + content_name, "w")
+    f.write(new_blog)
+    f.close()
+
+main()
